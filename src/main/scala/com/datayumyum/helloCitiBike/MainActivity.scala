145ee482507a6de093da65722ad051d1921dd646
sto <Son.C.To@gmail.com>
2014-04-25 16:53:18 -0400

    scala.util.parsing.json.JSON causes stackover flow on android but works on regular JVM

--- src/main/scala/com/datayumyum/helloCitiBike/MainActivity.scala
+++ src/main/scala/com/datayumyum/helloCitiBike/MainActivity.scala
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity
 import android.location.Location
 import com.google.android.gms.maps.model.{BitmapDescriptorFactory, MarkerOptions, LatLng}
 import com.google.android.gms.location.LocationClient
+import scala.util.parsing.json.JSON
 
 class MainActivity extends FragmentActivity {
   val TAG = "com.datayumyum.helloCitiBike.MainActivity"
@@ -25,16 +26,13 @@ class MainActivity extends FragmentActivity {
     initializeLocationClient()
   }
 
-  def zoomTo(location: Location): Unit = {
-    val latLng: LatLng = new LatLng(location.getLatitude(), location.getLongitude())
+  def zoomTo(latLng: LatLng): Unit = {
     val cameraUpdate: CameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15)
     val map = getMap()
-    map.animateCamera(cameraUpdate);
+    map.animateCamera(cameraUpdate)
   }
 
-  def addMarker() {
-    val location = locationClient.getLastLocation()
-    val xy = new LatLng(location.getLatitude(), location.getLongitude())
+  def addMarker(xy: LatLng) {
     val map = getMap()
     val bikeIcon = BitmapDescriptorFactory.fromResource(R.drawable.bike)
 
@@ -67,9 +65,51 @@ class MainActivity extends FragmentActivity {
 
     override def onConnected(bundle: Bundle): Unit = {
       Log.d(TAG, f"onConnected")
-      zoomTo(locationClient.getLastLocation())
-      addMarker()
+      val lastLocation: Location = locationClient.getLastLocation()
+      val xy = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude())
+      zoomTo(xy)
+      addMarker(xy)
+      addStations()
+    }
+
+    def addStations() {
+      thread {
+        val jsonStr = scala.io.Source.fromURL("http://appservices.citibikenyc.com/data2/stations.php").getLines.mkString("\n")
+        val jsonAST = JSON.parseFull(jsonStr)
+        val parsedMap: Map[String, List[Map[String, Any]]] = jsonAST.get.asInstanceOf[Map[String, List[Map[String, Any]]]]
+        val results: List[Map[String, Any]] = parsedMap("results")
+
+        val stations: List[Station] = results.map {
+          stationMap =>
+            Station(stationMap("id").asInstanceOf[Double].toInt,
+              stationMap("label").asInstanceOf[String],
+              stationMap("latitude").asInstanceOf[Double],
+              stationMap("longitude").asInstanceOf[Double],
+              stationMap("availableBikes").asInstanceOf[Double].toInt,
+              stationMap("availableDocks").asInstanceOf[Double].toInt)
+        }
+        stations.foreach {
+          station => val xy = new LatLng(station.latitude, station.longitude)
+            uiThread {
+              addMarker(xy)
+            }
+        }
+      }
     }
   }
 
+  def thread[F](f: => F) = (new Thread(new Runnable() {
+    def run() {
+      f
+    }
+  })).start
+
+  def uiThread[F](f: => F) = runOnUiThread(new Runnable() {
+    def run() {
+      f
+    }
+  })
 }
+
+
+case class Station(id: Int, label: String, latitude: Double, longitude: Double, availableBikes: Int, availableDocks: Int)
